a9ee0f9c643f391e1b9a06eab379f94470002d6a
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-07-06 18:19:47 -0400

    Add new image to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './refresh.svg';
+import Icon2 from './more_vert.svg';
 
 function component() {
   const element = document.createElement('div');
@@ -12,7 +13,7 @@ function component() {
   // Add the image to our existing div.
 
   const myIcon = new Image();
-
+  
   myIcon.src = Icon;
   myIcon.alt = "Refresh";
 
@@ -21,6 +22,24 @@ function component() {
 }
 
 document.getElementById('theTop').appendChild(component());
+
+function component2() {
+  const element2 = document.createElement('div');
+
+  //  // Lodash, now imported by this script
+  //  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  //  element.classList.add('hello');
+
+  // Add the image to our existing div.
+
+  const vertIcon = new Image();
+  
+  vertIcon.src = Icon2;
+  vertIcon.alt = "Vertical dots";
+
+  element2.appendChild(vertIcon);
+  return element2;
+}
 
 const tasks = [
   {
@@ -57,7 +76,7 @@ function loadHTML() {
   sortTasks();
 
   for (let i = 0; i < tasks.length; i++) {
-    superHTML.insertAdjacentHTML("beforeend", `<li class="noBullet"><input type="checkbox">${tasks[i].description}</li>`);
+    superHTML.insertAdjacentHTML("beforeend", `<li class="noBullet bottomBorder taskli"><input type="checkbox">${tasks[i].description}</li>`);
   }
 }
 
@@ -66,3 +85,5 @@ function loadHTML() {
 // }
 
 window.addEventListener('load', loadHTML);
+
+document.getElementById('experiment1').appendChild(component2());
